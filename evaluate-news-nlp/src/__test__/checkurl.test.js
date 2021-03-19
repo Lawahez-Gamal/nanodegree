4bee222b5a6dbdad1e34b549cb098dce08fce3bb
Lawahez-Gamal <lelyelsamahy@gmail.com>
2021-03-20 00:43:39 +0200

    nano project

---
+++ evaluate-news-nlp/src/__test__/checkurl.test.js
@@ -0,0 +1,5 @@
+const urlCheck = require('../client/js/checkurl')
+
+test('the fuction to be defined', () => {
+    expect(urlCheck.urlValidator).toBeDefined();
+})
--- evaluate-news-nlp/src/client/js/checkurl.js
+++ evaluate-news-nlp/src/client/js/checkurl.js
@@ -1,7 +1,7 @@
-function urlValidator(uri) {
-    console.log( uri);
+function urlValidator(url) {
+    console.log( url);
     let regexp =  /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-    if(regexp.test(uri)){
+    if(regexp.test(url)){
         return true;
     }else{
         return false;
